97e0c7e8fa2d541cdeb46a429d23a68c2519c9eb
jefferson <jeffinhodelph@hotmail.com>
2024-06-04 12:51:26 -0400

    improvement: replace mocked data by db data for realtors info

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from listings.models import Listing
+from realtors.models import Realtor
 
 def index(request):
     listings = Listing \
@@ -9,7 +10,16 @@ def index(request):
         .order_by('-list_date') \
         .filter(is_published=True)[:3]
 
-    return render(request, "pages/index.html", context={'listings': listings})
+    return render(request, "pages/index.html", context={"listings": listings})
 
 def about(request):
-    return render(request, "pages/about.html")
+    realtors = Realtor \
+        .objects \
+        .order_by('-hire_date')
+
+    mvp_realtors = Realtor \
+        .objects \
+        .all() \
+        .filter(is_mvp=True)
+
+    return render(request, "pages/about.html", context={"realtors": realtors, "mvp_realtors": mvp_realtors})
