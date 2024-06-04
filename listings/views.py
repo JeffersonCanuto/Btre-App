df387d3cf7d374c0f9b2c708df919dbe9b9bf5f8
jefferson <jeffinhodelph@hotmail.com>
2024-06-04 12:14:13 -0400

    improvement: replace mocked data by db data on latest listings

--- listings/views.py
+++ listings/views.py
@@ -14,11 +14,7 @@ def index(request):
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
 
-    context = {
-        'listings': paged_listings
-    }
-
-    return render(request, 'listings/listings.html', context)
+    return render(request, 'listings/listings.html', context={'listings': paged_listings})
 
 def listing(request, listing_id):
     return render(request, 'listings/listing.html')
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Listing
+
 def index(request):
-    return render(request, "pages/index.html")
+    listings = Listing \
+        .objects \
+        .order_by('-list_date') \
+        .filter(is_published=True)[:3]
+
+    return render(request, "pages/index.html", context={'listings': listings})
 
 def about(request):
     return render(request, "pages/about.html")
