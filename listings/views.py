0470e6d2630b37a529896435089a059283d7a003
jefferson <jeffinhodelph@hotmail.com>
2024-06-17 10:19:52 -0400

    refactor: dynamically loop through bedroom, price and state options in listings app

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
+from .choices import bedroom_choices, price_choices, state_choices
+
 from .models import Listing
 
 # Create your views here.
@@ -14,7 +16,11 @@ def index(request):
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
 
-    return render(request, 'listings/listings.html', context={'listings': paged_listings})
+    context = {
+        'listings': paged_listings
+    }
+
+    return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
     listing = get_object_or_404(Listing, pk=listing_id)
@@ -22,4 +28,10 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context={'listing': listing})
 
 def search(request):
-    return render(request, 'listings/search.html')
+    context = {
+        "bedroom_choices": bedroom_choices,
+        "price_choices": price_choices,
+        "state_choices": state_choices
+    }
+
+    return render(request, 'listings/search.html', context)
