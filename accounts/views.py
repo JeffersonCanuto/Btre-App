3b9e6c2bb32d02e72de2c7d7d6237dfcf33fc886
jefferson <jeffinhodelph@hotmail.com>
2024-06-25 10:23:01 -0400

    feat: add register and login templates and routes

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 def register(request):
+    if request.method == 'POST':
+        messages.error(request, 'Testing error message')
+        return redirect('register')
+    
     return render(request, 'accounts/register.html')
 
 def login(request):
+    if request.method == 'POST':
+        # Register User
+        print("Login User!")
+    
     return render(request, 'accounts/login.html')
 
 def logout(request):
