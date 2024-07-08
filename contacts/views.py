779d80b7371bd30e5dca3e3e55d6ced9a3fc7b60
jefferson <jeffinhodelph@hotmail.com>
2024-07-08 12:58:00 -0400

    feat: implement contact form submission and alerts

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Contact
 
 # Create your views here.
 def contact(request):
-    return
+    if request.method == 'POST':
+        # Get form values
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        realtor_email = request.POST['realtor_email']
+        
+        # Save form values on the database
+        contact = Contact(listing=listing, listing_id=listing_id, \
+            name=name, email=email, phone=phone, message=message, user_id=user_id)
+        contact.save()
+
+        # Trigger success message after form submission
+        messages.success(request, 'Your request has been submitted. A realtor will get back to you soon!')
+
+        # Redirect to single listing page
+        return redirect(f'/listings/{listing_id}')
