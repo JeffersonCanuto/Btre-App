a3cf209ebc4c7ef348c4032ecbe4fa21db5284ac
jefferson <jeffinhodelph@hotmail.com>
2024-07-08 13:52:02 -0400

    feat: prevent authenticated users from making multiple inquiries for the same listing

--- contacts/views.py
+++ contacts/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Contact
 
+from django.contrib.auth.models import User
+
 # Create your views here.
 def contact(request):
-    if request.method == 'POST':
+    if request.method == 'POST':    
         # Get form values
         listing_id = request.POST['listing_id']
         listing = request.POST['listing']
@@ -15,13 +17,25 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
         
-        # Save form values on the database
+        # Check whether current user id has already made an inquiry for the current listing id
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+        
+            if has_contacted:
+                # Trigger error message in case there is an inquiry for the current listing
+                messages.error(request, 'You have already made an inquiry for this listing!')
+
+                # Redirect to single listing page
+                return redirect(f'/listings/{listing_id}')
+        
+        # Save contact form values on the database
         contact = Contact(listing=listing, listing_id=listing_id, \
             name=name, email=email, phone=phone, message=message, user_id=user_id)
         contact.save()
 
         # Trigger success message after form submission
         messages.success(request, 'Your request has been submitted. A realtor will get back to you soon!')
-
+        
         # Redirect to single listing page
         return redirect(f'/listings/{listing_id}')
